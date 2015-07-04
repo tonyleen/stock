c6fdf73d5e75472efa4d817889b8ecaeb95454b0
Tony <tonyleen@live.com>
2015-07-04 14:25:45 +0800

    init

---
+++ chat.js
@@ -0,0 +1,20 @@
+﻿var StockInfo = require('./main.js');
+var defauleStockinfo 
+var init = function (server) {
+    var io = require('socket.io')(server);
+    io.on('connection', function (socket) {
+        socket.on('say', function (words) {
+            if (typeof words !== 'string') return;
+            socket.broadcast.emit('said', words);
+            console.log('some one said', words);
+        });
+        //console.log('some one login');
+    });
+    setInterval(function () {
+        StockInfo.GetStockInfo('sh000001', 'minute', function (err, result) {
+            if (err) return;
+            if (result) io.emit('stockinfo', { code: 'sh000001', info: result });
+        });
+    }, 1000 * 10);
+};
+module.exports = function (server) { init(server); };
---
+++ main.js
@@ -0,0 +1,6 @@
+﻿var request = require('request');
+exports.GetStockInfo = function (code, type, callback) {
+    request.get('http://data.gtimg.cn/flashdata/hushen/' + type + '/' + code + '.js?' + Math.random(), {}, function (err, result) {
+        callback(err, result.body);
+    });
+};
